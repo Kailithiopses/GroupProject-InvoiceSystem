4bdc80650f5eeacc470d79934a9f46de86991969
Kailithiopses <connereames@mail.weber.edu>
2026-07-23 11:49:04 -0600

    Made final prototype changes for search window. Added outline of clsSearchLogic class, as well as some comments in xaml.cs file and xaml file

--- InvoiceSystem/Search/wndSearch.xaml.cs
+++ InvoiceSystem/Search/wndSearch.xaml.cs
@@ -18,12 +18,65 @@ namespace InvoiceSystem.Search
     public partial class wndSearch : Window
     {
 
+        // Instantiate SearchLogic class
+
         // Create a list of invoices populated from the data base
         public wndSearch()
         {
             InitializeComponent();
             // Populate the datagrid using the list of invoices
             // dgInvoiceDisplay.ItemsSource = InvoicesManager.GetInvoices();
+
+            // Populate combo boxes with distinct values for each filter
+        }
+
+        /// <summary>
+        /// Handle logic for cancel button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Close this window and return to main menu without setting sSelectedInvoice
+        }
+
+        /// <summary>
+        /// Handle logic for clicking select invoice button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelectInvoice_Click(object sender, RoutedEventArgs e)
+        {
+            // This button will be disabled by default. It will only be enabled if the user
+            // has selected an item from the datagrid
+
+            // Get the selected item from the datagrid
+            // sSelectedInvoice = dgInvoiceDisplay.SelectedItem
+            // Close search window and return to main window
+            
+        }
+
+        /// <summary>
+        /// Handle logic for clicking clear filter button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnClearFilter_Click(object sender, RoutedEventArgs e)
+        {
+            // Programmatically change the selected items in all 3 combo boxes to null
+            // comboBox.SelectedIndex = -1; // this should clear the selection from the box, returning null
+            // Selection changed method will automatically call SQL queries to update the datagrid
+        }
+
+        /// <summary>
+        /// Handle logic for when a combo box selection is changed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Call GetInvoices method, pass in the values of each combo box
+            // dgInvoiceDisplay.ItemsSource = GetInvoices(cbInvoiceNumFilter.SelectedItem, cbInvoiceDateFilter.SelectedItem, cbInvoiceTotalCostFilter.SelectedItem)
         }
 
 

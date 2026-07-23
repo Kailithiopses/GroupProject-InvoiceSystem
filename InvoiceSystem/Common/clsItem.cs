a70db9c0cf9f1dac2816757f76ba0a450764d42d
Kailithiopses <connereames@mail.weber.edu>
2026-07-23 11:08:17 -0600

    Added try catch blocks to SQL methods, implemented structure for clsItem objects

--- InvoiceSystem/Common/clsItem.cs
+++ InvoiceSystem/Common/clsItem.cs
@@ -6,5 +6,15 @@ namespace InvoiceSystem.Common
 {
     internal class clsItem
     {
+
+        // This class holds the code, description, and cost of a given item
+
+        public string ItemCode { get; set; }
+
+        public string ItemDesc { get; set; }
+
+        public string Cost { get; set; }
+
+
     }
 }
